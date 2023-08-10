f5864418365e062ae454576a1795aec2b8304471
Luis Felipe Ribeiro <44618496+felipert2@users.noreply.github.com>
2023-08-10 12:34:42 -0300

    primeira funcao

---
+++ main.js
@@ -0,0 +1,21 @@
+let produtoEscolhido = prompt("Digite seu produto desejado, Tenis, Caneca, Camisetas")
+
+produto();
+
+function produto()
+{
+    if(produtoEscolhido == "Tenis"){
+        alert("Aguarde Novidades de Tenis Chegando")        
+    }else if(produtoEscolhido == "Caneca"){
+        alert("Aguarde Novidades de Canecas Chegando")        
+    }else if(produtoEscolhido == "Camisetas"){
+        alert("Aguarde Novidades de Camisetas Chegando")        
+    }
+    
+    
+    
+    else{
+        alert("Infelizmente não temos esse produto")
+    }
+}
+
